05bb9b6a6958c28a4686a964abf07c60639b5f3b
KevCast28 <kev.dev0928@gmail.com>
2026-06-16 01:20:57 -0500

    feat: implemented CRUD for Products

--- src/main/java/com/deliciasmolina/deliciasmolinaapi/service/interfaces/ProductService.java
+++ src/main/java/com/deliciasmolina/deliciasmolinaapi/service/interfaces/ProductService.java
@@ -1,4 +1,18 @@
 package com.deliciasmolina.deliciasmolinaapi.service.interfaces;
 
+import com.deliciasmolina.deliciasmolinaapi.entity.Product;
+
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getAll();
+
+    Product getById(Long id);
+
+    Product create(Product product);
+
+    Product update(Long id, Product product);
+
+    void delete(Long id);
 }
