e32dfc457873e9a46b242cd48143a878840fa1cc
KevCast28 <kev.dev0928@gmail.com>
2026-06-16 01:27:52 -0500

    Revert "feat: implemented CRUD for Products"
    This reverts commit 05bb9b6a6958c28a4686a964abf07c60639b5f3b.

--- src/main/java/com/deliciasmolina/deliciasmolinaapi/entity/Product.java
+++ src/main/java/com/deliciasmolina/deliciasmolinaapi/entity/Product.java
@@ -3,10 +3,6 @@ package com.deliciasmolina.deliciasmolinaapi.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -15,10 +11,6 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "products")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Product {
 
     @Id
