a9e136aa998ea8b228d642eb1da1c4decadf184a
KevCast28 <kevinsn524@gmail.com>
2026-06-16 23:42:46 -0500

    feat: created service for Orders

--- src/main/java/com/deliciasmolina/deliciasmolinaapi/repository/ProductRepository.java
+++ src/main/java/com/deliciasmolina/deliciasmolinaapi/repository/ProductRepository.java
@@ -4,4 +4,5 @@ import com.deliciasmolina.deliciasmolinaapi.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
--- src/main/java/com/deliciasmolina/deliciasmolinaapi/service/interfaces/OrderService.java
+++ src/main/java/com/deliciasmolina/deliciasmolinaapi/service/interfaces/OrderService.java
@@ -1,6 +1,17 @@
 package com.deliciasmolina.deliciasmolinaapi.service.interfaces;
 
+import com.deliciasmolina.deliciasmolinaapi.entity.Order;
+
+import java.util.List;
+
 public interface OrderService {
+    List<Order> getAll();
 
+    Order getById(Long id);
 
+    Order create(Order order);
+
+    Order update(Long id, Order order);
+
+    void detele(Long id);
 }
